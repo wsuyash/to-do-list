f75c36b97f75a54eea9228d30cc9818ca87d9dc7
wsuyash <suyashcr07@gmail.com>
2021-11-05 11:10:17 +0530

    Added some comments and formatted code

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express'); // Importing Express
 const app = express(); // Using Express
-const port = 8000;
+const port = 8000; // Setting port to 8000
 
 // Getting the database
 const db = require('./config/mongoose');
@@ -21,51 +21,3 @@ app.listen(port, function(err){
 
     console.log(`Server up and running on port: ${port}`);
 });
-
-// Adding a task to the db
-/* app.post('/new-task', function(req, res){
-    Tasks.create({
-        description: req.body.description,
-        category: req.body.category,
-        due_date: req.body.due_date
-    }, function(err, newTask){
-        if(err) {
-            console.log(`Error adding task to the db: ${err}`);
-            return;
-        }
-    });
-
-    return res.redirect('back');
-});
-
-// Deleting task(s)
-app.post('/delete-task', function(req, res){
-    let checkboxes = req.body.checkbox;
-
-    if(checkboxes !== undefined) { // Check to see if no task is selected
-        for(let i = 0; i < checkboxes.length; i++){
-            Tasks.findByIdAndDelete(checkboxes, function(err){
-                if(err){
-                    console.log('Error deleting from db');
-                }
-            });
-        }
-    }
-
-    return res.redirect('back');
-});
-
-// Render the home page and display tasks
-app.get('/', function(req, res){
-    Tasks.find({}, function(err, tasks){
-        if(err){
-            console.log('Error fetching tasks from the db.');
-            return;
-        }
-        return res.render('home', 
-        {
-            title: "To-Do List",
-            task_list: tasks
-        });
-    });
-}); */
--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,12 @@ const router = express.Router();
 const db = require('../config/mongoose');
 const Tasks = require('../models/task');
 
+// Importing all the controllers
 const homeController = require('../controllers/home_controller');
 const addTask = require('../controllers/new_task_controller');
 const deleteTask = require('../controllers/delete_task_controller');
 
+// Routing to appropriate controller functions
 router.get('/', homeController.home);
 router.post('/new-task', addTask.new_task);
 router.post('/delete-task', deleteTask.delete_task);
